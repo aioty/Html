e3ad4452b5a7dbaab0740bff5a697e479f52752a
zzy <591702243@qq.com>
2016-08-21 14:41:39 +0800

    2016-8-21

---
+++ report.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>report</title>
+</head>
+<?php
+
+$name=$_POST['name'];     //名字
+$xuehao=$_POST['xuehao'];   //学号
+$banji=$_POST['banji'];    //班级
+$nianji=$_POST['nianji'];   //年级
+$youxiang=$_POST['youxiang']; //邮箱
+$shouji=$_POST['shouji'];   //手机
+$zubie=$_POST['zubie'];    //组别
+
+//获得数据后。。
+echo "thanks to you!";
+
+?>
+<body>
+</body>
+</html>
---
+++ 错误修正笔记.c
@@ -0,0 +1,8 @@
+//错误1：
+	html5和html4及以下的html文档类型声明是不同的，html5中只需要写<!doctype html> ,而html4及以下的写法比较繁杂，种类要多。
+比如<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/RT/html4/loose.dtd">等等、
+
+//错误2：
+	html中的注释形式是<!--xxx--> 而不是<!--xxx----!> 特别注意了，上次运行没有实现相应的显示估计就是这个问题。
+	
+//错误3：
